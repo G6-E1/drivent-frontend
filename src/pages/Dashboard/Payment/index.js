6fda219c5d0b4d965df5c86d33f2e82c3f9bbe28
Hugo Gomes <gomes.hugo00@gmail.com>
2023-03-02 16:55:43 -0300

    feat: layout and display logic to the feedback online modality.

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,72 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  const [display, setDisplay] = useState('none');
+  function chooseModality() {
+    if (display === 'none') {
+      setDisplay('flex');
+    } else {
+      setDisplay('none');
+    }
+  }
+
+  return (
+    <>
+      <button onClick={chooseModality}>clicka ai</button>
+      {onlineFeedback(100, display)}
+    </>
+  );
+}
+
+function onlineFeedback(price, display) {
+  const Screen = styled.div`
+    display: ${(props) => props.display};
+    flex-direction: column;
+    gap: 20px;
+    padding: 10px;
+  `;
+
+  const Feedback = styled.div`
+    font-size: 22px;
+    color: gray;
+
+    span {
+      font-weight: bold;
+    }
+  `;
+
+  const Button = styled.button`
+    height: 40px;
+    width: 220px;
+    color: black;
+    font-weight: bold;
+    font-family: sans-serif;
+    text-transform: uppercase;
+    background-color: rgba(215, 215, 215, 1);
+    border-radius: 8px;
+    box-shadow: 0px 0px 3px gray;
+    cursor: pointer;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      box-shadow: 0px 0px 3px black;
+    }
+  `;
+
+  function reserveTicket() {
+    // Lógica pra reservar do ticket
+  }
+
+  return (
+    <Screen display={display}>
+      <Feedback>
+        Fechado! O total ficou em <span>R$ {price}</span>. Agora é só confirmar:
+      </Feedback>
+      <Button onClick={reserveTicket}>reservar ingresso</Button>
+    </Screen>
+  );
 }
