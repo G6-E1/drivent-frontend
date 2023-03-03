9045b7b3fe56ddfad771151a819bb6e94d6d9eb0
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-03 12:37:12 -0300

    feat: select ticketType

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,16 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import TicketType from '../../../components/Dashboard/Payment/TicketType';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Ingressos e pagamento</StyledTypography>
+      <TicketType/>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px!important;
+`;
