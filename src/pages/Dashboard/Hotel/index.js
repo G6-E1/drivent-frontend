47ed24a9742e44cbcfddcd32870bfa9f7b2e580c
JoaoVitorRP <joaovrp.mec.ufsc@gmail.com>
2023-03-08 12:36:09 -0300

    feat: static hotel summary

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
 import SummaryRoom from '../../../components/Dashboard/Hotel/SummaryRoom';
 import HotelList from '../../../components/Dashboard/Hotel/HotelList';
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
 
 export default function Hotel() {
-  const [showSummaryRoom, setShowSummaryRoom] = useState(false);
+  const [showSummaryRoom, setShowSummaryRoom] = useState(true);
 
   return (
     <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
       {showSummaryRoom ? (
         <SummaryRoom setShowSummaryRoom={setShowSummaryRoom} />
       ) : (
@@ -15,3 +18,7 @@ export default function Hotel() {
     </>
   );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 36px !important;
+`;
