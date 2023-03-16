0c2e176456ecf76df59a8cfc2f2cd0f0fa41de25
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-16 17:28:35 -0300

    feat: buttons to select day

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,16 @@
+import Days from '../../../components/Dashboard/Activities/Days';
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
 export default function Activities() {
-  return 'Atividades: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
+      <Days />
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 36px !important;
+`;
