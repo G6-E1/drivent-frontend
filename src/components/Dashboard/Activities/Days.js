459407fca395df51692520e1225060dc904c84c4
rafaelBporto <rafaelbporto88@gmail.com>
2023-03-17 16:53:47 -0300

    feat: send daySelect to index(Activities)

--- src/components/Dashboard/Activities/Days.js
+++ src/components/Dashboard/Activities/Days.js
@@ -3,39 +3,53 @@ import { useState } from 'react';
 import styled from 'styled-components';
 import useGetActiviesDates from '../../../hooks/api/useActivities';
 import Day from './Day';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+dayjs.locale('pt-br');
 
-const Activities = [
-  { startAt: '2023-03-06T17:33:28.062Z' },
-  { startAt: '2023-03-23 09:00:00' },
-  { startAt: '2023-03-24 09:00:00' },
-];
-
-export default function Days() {
+export default function Days({ setdate }) {
   const { getDateActivities } = useGetActiviesDates();
 
   const [selectedDay, setSelectedDay] = useState();
-  const [activities, setActivities] = useState();
+  const datesMap = new Map();
+  const datesArray = [];
+  const [dates, setDates] = useState();
+
   useEffect(() => {
+    let activities;
     async function fetchData() {
       try {
-        setActivities(await getDateActivities());
+        activities = await getDateActivities();
+        if (activities) {
+          uniqueDates(activities);
+        };
       } catch (error) {
-
+        console.log(error);
       }
     }
     fetchData();
   }, []);
 
-  console.log(activities);
+  function uniqueDates(activities) {
+    activities.map(date => {
+      const datePTBr = dayjs(date.startAt).format('ddd, DD/MM');
+      datesMap.set(datePTBr, { ptBr: datePTBr, startAt: date.startAt });
+    });
+    datesMap.forEach((d) => datesArray.push(d));
+    setDates(datesArray);
+  }
+  console.log(dates);
   return (
     <ContainerDays>
-      {activities?.map((day, i) => <Day
+      {dates?.map((d, i) => <Day
         key={i}
         idButton={i}
-        day={day.startAt}
+        day={d}
         selectedDay={selectedDay}
         setSelectedDay={setSelectedDay}
-      />)}
+        setdate={setdate}
+      />)
+      }
     </ContainerDays>
   );
 };
